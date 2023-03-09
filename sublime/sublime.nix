557be19bc5a510a649c51e8d4ac4295ff26928d6
Sebastian Porto <s@porto5.com>
2023-03-09 12:09:45 +1100

    Add Sublime prefs

--- sublime/sublime.nix
+++ sublime/sublime.nix
@@ -4,11 +4,18 @@
 
 
 let
-  targetPath =
-    "/Library/Application Support/Sublime Text/Packages/User/Default (OSX).sublime-keymap";
+  macPackageUserPath =
+     "/Library/Application Support/Sublime Text/Packages/User/";
+  macKeyBindings =
+    "Default (OSX).sublime-keymap";
+  preferences =
+    "Preferences.sublime-settings";
 in
 {
-  home.file."${targetPath}" = {
-    source = ./mac.sublime-keymap;
+  home.file."${macPackageUserPath}${macKeyBindings}" = {
+    source = ./DefaultOSX.sublime-keymap;
+  };
+  home.file."${macPackageUserPath}${preferences}" = {
+    source = ./Preferences.sublime-settings;
   };
 }
